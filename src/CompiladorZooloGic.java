c9244484d56e48d0c69c4c60945a4e9bcdfa2c3a
Danilo Chagas <danilovga1000@gmail.com>
2024-08-24 22:32:27 -0300

    att falta so verificacao de tipo

--- src/CompiladorZooloGic.java
+++ src/CompiladorZooloGic.java
@@ -24,7 +24,7 @@ public class CompiladorZooloGic {
 
         walker.walk(listener, ast);
 
-        System.out.println(listener.getSymbolTable().toString());
+        System.out.println(listener.getScopeStack().toString());
     }
 
 
--- src/MyListener.java
+++ src/MyListener.java
@@ -1,59 +1,118 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class MyListener extends GrammarZooLogicBaseListener{
+public class MyListener extends GrammarZooLogicBaseListener {
 
-    private Map<String,String> symbolTable = new HashMap<>();
+    private final Map<String, String> symbolTable = new HashMap<>();
+    private final Deque<Map<String, String>> scopeStack = new ArrayDeque<>();
 
-//    @Override
-//    public void enterEveryRule(ParserRuleContext ctx) {
-//        System.out.println("Entrou na regra: " + ctx.getText());
-//    }
-//
-//    @Override
-//    public void exitEveryRule(ParserRuleContext ctx) {
-//        System.out.println("Saiu da regra: " + ctx.getText());
-//    }
+    public MyListener() {
+        // Inicializa o primeiro escopo global
+        scopeStack.push(new HashMap<>());
+    }
 
     @Override
     public void enterNDeclaracao(GrammarZooLogicParser.NDeclaracaoContext ctx) {
-        System.out.println("Entrou na regra: " + ctx.getText());
+        String varName = ctx.VAR().getText();
+        String varType = ctx.TIPO().getText();
+
+        if (currentScope().containsKey(varName)) {
+            throw new RuntimeException("Erro: Variável '" + varName + "' já foi declarada.");
+        }
+
+        currentScope().put(varName, varType);
     }
 
     @Override
-    public void exitNDeclaracao(GrammarZooLogicParser.NDeclaracaoContext ctx) {
+    public void enterNAtribuicao(GrammarZooLogicParser.NAtribuicaoContext ctx) {
+        String varName = ctx.VAR().getText();
 
-        System.out.println("Saiu da regra: " + ctx.getText());
+        if (!isVariableDeclared(varName)) {
+            throw new RuntimeException("Erro: Variável '" + varName + "' não foi declarada.");
+        }
+    }
 
-        //Verificando se tem variável já declarada
-        String var = ctx.VAR().getText();
-        String tipo = ctx.TIPO().getText();
-        if(symbolTable.containsKey(var)){
-            System.out.println("Variável " + var + " já declarada");
-        } else {
-            symbolTable.put(var, tipo);
+    @Override
+    public void enterNFuncao(GrammarZooLogicParser.NFuncaoContext ctx) {
+        String funcName = ctx.VAR().getText();
+
+        if (currentScope().containsKey(funcName)) {
+            throw new RuntimeException("Erro: Função '" + funcName + "' já foi declarada.");
         }
 
+        currentScope().put(funcName, ctx.TIPO().getText());
+
+        // Novo escopo para os parâmetros e variáveis locais da função
+        scopeStack.push(new HashMap<>());
     }
 
-//    @Override
-//    public void enterNAtribuicao(GrammarZooLogicParser.NAtribuicaoContext ctx) {
-//        System.out.println("Entrou na regra: " + ctx.getText());
-//    }
-//
-//    @Override
-//    public void exitNAtribuicao(GrammarZooLogicParser.NAtribuicaoContext ctx) {
-//
-//        System.out.println("Saiu da regra: " + ctx.getText());
-//
-//    }
-
-
-
-
-    public Map<String, String> getSymbolTable() {
-        return symbolTable;
+    @Override
+    public void exitNFuncao(GrammarZooLogicParser.NFuncaoContext ctx) {
+        // Retira o escopo da função
+        scopeStack.pop();
     }
 
+    @Override
+    public void enterParam(GrammarZooLogicParser.ParamContext ctx) {
+        String paramName = ctx.VAR().getText();
+        String paramType = ctx.TIPO().getText();
 
+        if (currentScope().containsKey(paramName)) {
+            throw new RuntimeException("Erro: Parâmetro '" + paramName + "' já foi declarado.");
+        }
+
+        currentScope().put(paramName, paramType);
+    }
+
+    @Override
+    public void enterNIf(GrammarZooLogicParser.NIfContext ctx) {
+        // Novo escopo para o bloco 'if'
+        scopeStack.push(new HashMap<>());
+    }
+
+    @Override
+    public void exitNIf(GrammarZooLogicParser.NIfContext ctx) {
+        // Retira o escopo do bloco 'if'
+        scopeStack.pop();
+    }
+
+    @Override
+    public void enterNWhile(GrammarZooLogicParser.NWhileContext ctx) {
+        // Novo escopo para o bloco 'while'
+        scopeStack.push(new HashMap<>());
+    }
+
+    @Override
+    public void exitNWhile(GrammarZooLogicParser.NWhileContext ctx) {
+        // Retira o escopo do bloco 'while'
+        scopeStack.pop();
+    }
+
+    @Override
+    public void enterNFOR(GrammarZooLogicParser.NFORContext ctx) {
+        // Novo escopo para o bloco 'for'
+        scopeStack.push(new HashMap<>());
+    }
+
+    @Override
+    public void exitNFOR(GrammarZooLogicParser.NFORContext ctx) {
+        // Retira o escopo do bloco 'for'
+        scopeStack.pop();
+    }
+
+    private boolean isVariableDeclared(String varName) {
+        for (Map<String, String> scope : scopeStack) {
+            if (scope.containsKey(varName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Map<String, String> currentScope() {
+        return scopeStack.peek();
+    }
+
+    public Deque<Map<String, String>> getScopeStack() {
+        return scopeStack;
+    }
 }
